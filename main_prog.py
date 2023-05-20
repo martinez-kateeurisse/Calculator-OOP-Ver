cbbcb71c355f94b1afff1c1e8ee492996eb10b7d
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-20 15:16:53 +0800

    Main program file - created

---
+++ main_prog.py
@@ -0,0 +1,5 @@
+#Kate Eurisse Martinez_BSCPE 1-5_Simple App Calculator - OOP Version
+
+#This program will create a Simple App Calculator; where the user will input the operation as well as the two numbers that they want to calculate.
+#The program will use exception handling (try-catch method) to handle user input errors.
+#The program will be executed in an Object oriented programming way
