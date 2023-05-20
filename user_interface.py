da5cffe4362096d9b6d39e6ee421f0c36618ae11
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-20 16:29:44 +0800

    UI file: Ask user for numbers

--- user_interface.py
+++ user_interface.py
@@ -8,8 +8,14 @@ class UserInterface:
         #Create instance variables
         #Ask user for Operation
         operation = input("Please enter the operation: ")
+        #Return the value
         return operation
     #Numbers
+    def num_input(self):
         #Ask user for numbers
+        num = float(input("Please input the first number: "))
+        #Return the value
+        return num
     #Retry
         #Ask users if they want to try again
+        #Return the value
