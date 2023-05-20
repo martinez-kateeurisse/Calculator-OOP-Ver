5d0531d87b52d908ef4f655a7ac5694d8f5d6a5d
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-20 15:52:31 +0800

    UI File: Class and user input object - created

--- user_interface.py
+++ user_interface.py
@@ -1,1 +1,7 @@
 #This class file will include the codes that require a user interface.
+
+#Create class
+class UserInterface:    
+#Create objects
+    def user_input(self):
+#Create instance variables
