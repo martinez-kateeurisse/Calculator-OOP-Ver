6834eecb478c1256d07456c40f8adbb3936ae656
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-20 15:42:45 +0800

    Pseudocode

--- calc_main.py
+++ calc_main.py
@@ -2,4 +2,12 @@
 
 #This program will create a Simple App Calculator; where the user will input the operation as well as the two numbers that they want to calculate.
 #The program will use exception handling (try-catch method) to handle user input errors.
-#The program will be executed in an Object oriented programming way
+#The program will be executed in an Object oriented programming way 
+
+#Ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication and Division)
+#Ask the user for two numbers
+#Display the result
+#Ask if the user wants to try again or not.
+#If yes, repeat Step 1.
+#If no, Display “Thank you!” and the program will exit 
+#Use Python Function and appropriate Exceptions to capture errors during runtime.
