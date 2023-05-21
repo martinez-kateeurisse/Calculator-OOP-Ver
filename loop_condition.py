c0b8380f2b954e94450f6c4b185d264627ae7326
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-21 21:17:53 +0800

    Change of plans - including design in the main prog instead of another class

--- loop_condition.py
+++ loop_condition.py
@@ -4,15 +4,15 @@ import tkinter as tk
 #Create class
 class LoopCondition:
 #Create Object
-    def retry_prog(self, retry):
+    #def retry_prog(self, retry):
     #Create variable instances(if-else condition)
-        if retry == 'y':        
+        #if retry == 'y':        
     #Return value
-            return retry
-        else:
-            print("Thank you!")
+           # return retry
+        #else:
+            #print("Thank you!")
     
-    def retry(input_num1, input_num2, input_operation,label_result):
+    def retry_prog(input_num1, input_num2, input_operation,label_result):
         input_num1.delete(0, tk.END)
         input_num2.delete(0, tk.END)
         input_operation.delete(0, tk.END)
--- prog_gui.py
+++ prog_gui.py
@@ -2,50 +2,52 @@
 import tkinter as tk 
 from loop_condition import LoopCondition
 loop = LoopCondition
+from calc_main import MainCalculator
+calc = MainCalculator
 #Create class
 class TkMethods:
 #Creeate Window
-    def tk_window(calculator):
-        calc_window = tk.Tk()
-        calc_window.title("Simple App Calculator") #Window Title
-        calc_window.geometry("400x300") #Window Size
-        calc_window.configure(bg="light cyan") #Window background#Create Labels
+    def tk_window(self):
+        self.calc_window = tk.Tk()
+        self.calc_window.title("Simple App Calculator") #Window Title
+        self.calc_window.geometry("400x300") #Window Size
+        self.calc_window.configure(bg="light cyan") #Window background#Create Labels
     #Ask the user to enter the operation
-        label_operator = tk.Label(calc_window, text="Operations (+, -, *, /):")
-        label_operator.grid(row=1, column=0, padx=10, pady=10)
+        self.label_operator = tk.Label(self.calc_window, text="Operations (+, -, *, /):")
+        self.label_operator.grid(row=1, column=0, padx=10, pady=10)
         #Ask the user to input the first number
-        label_num1 = tk.Label(calc_window, text="Please input the first number:")
-        label_num1.grid(row=2, column=0, padx=10, pady=10)
+        self.label_num1 = tk.Label(self.calc_window, text="Please input the first number:")
+        self.label_num1.grid(row=2, column=0, padx=10, pady=10)
         #Ask the user to input the second number
-        label_num2 = tk.Label(calc_window, text="Please input the second number:")
-        label_num2.grid(row=3, column=0, padx=10, pady=10)
+        self.label_num2 = tk.Label(self.calc_window, text="Please input the second number:")
+        self.label_num2.grid(row=3, column=0, padx=10, pady=10)
         #Ask the user if they want to try again
-        label_retry = tk.Label(calc_window, text="Do you want to try again?")
-        label_retry.grid(row = 6, column = 0, padx=10, pady=10)
+        self.label_retry = tk.Label(self.calc_window, text="Do you want to try again?")
+        self.label_retry.grid(row = 6, column = 0, padx=10, pady=10)
         #Displaying the result
-        label_result = tk.Label(calc_window, text="The answer is: ")
-        label_result.grid(row=5, column=0, columnspan=3, padx=10, pady=10)
+        self.label_result = tk.Label(self.calc_window, text="The answer is: ")
+        self.label_result.grid(row=5, column=0, columnspan=3, padx=10, pady=10)
     #Create Input Fields
         #Input for the operation
-        input_operation = tk.Entry(calc_window)
-        input_operation.grid(row=1, column=1, padx=10, pady=10)
+        self.input_operation = tk.Entry(self.calc_window)
+        self.input_operation.grid(row=1, column=1, padx=10, pady=10)
         #Input for the first number
-        input_num1 = tk.Entry(calc_window)
-        input_num1.grid(row=2, column=1, padx=10, pady=10)
+        self.input_num1 = tk.Entry(self.calc_window)
+        self.input_num1.grid(row=2, column=1, padx=10, pady=10)
         #Input for the second number
-        input_num2 = tk.Entry(calc_window)
-        input_num2.grid(row=3, column=1, padx=10, pady=10)
+        self.input_num2 = tk.Entry(self.calc_window)
+        self.input_num2.grid(row=3, column=1, padx=10, pady=10)
     #Create Buttons
         #Button for calculating the result
-        button_calculate = tk.Button(calc_window, text="Calculate the Result", command=calculator)
-        button_calculate.grid(row=4, column=1, padx=10, pady=10)
+        self.button_calculate = tk.Button(self.calc_window, text="Calculate the Result", command=calc.main_calculator)
+        self.button_calculate.grid(row=4, column=1, padx=10, pady=10)
         #Yes button - If yes, the program will repeat step 1
-        button_yes = tk.Button(calc_window, text="Yes", command=loop.retry)
-        button_yes.grid(row=4, column=1, padx=10, pady=10)
+        self.button_yes = tk.Button(self.calc_window, text="Yes", command=loop.retry)
+        self.button_yes.grid(row=4, column=1, padx=10, pady=10)
         #No button - If no, the calculator program will exit
-        button_no = tk.Button(calc_window, text="No", command=loop.quit_calculator)
-        button_no.grid(row=4, column=2, padx=10, pady=10)
+        self.button_no = tk.Button(self.calc_window, text="No", command=loop.quit_calculator)
+        self.button_no.grid(row=4, column=2, padx=10, pady=10)
     #Organize the buttons
-        button_calculate.grid(row=4, column=0, pady=10)
-        button_yes.grid(row=6, column=1, pady=10)
-        button_no.grid(row=6, column=2, pady=10)
+        self.button_calculate.grid(row=4, column=0, pady=10)
+        self.button_yes.grid(row=6, column=1, pady=10)
+        self.button_no.grid(row=6, column=2, pady=10)
