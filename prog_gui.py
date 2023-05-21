6abb43c07b168dcc025f5e6db9112837835a5472
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-21 19:11:34 +0800

    Prog gui File: window created

--- prog_gui.py
+++ prog_gui.py
@@ -1,6 +1,14 @@
 #This python file will have the methods relating to program's tkinter execution.
-
+import tkinter as tk 
+#Create class
+class tk_methods:
 #Creeate Window
+    def calc_window():
+        calc_window = tk.Tk()
+        calc_window.title("Simple App Calculator") #Window Title
+        calc_window.geometry("400x300") #Window Size
+        calc_window.configure(bg="light cyan") #Window background
+
 #Create Labels
 #Create Input Fields
 #Create Buttons
