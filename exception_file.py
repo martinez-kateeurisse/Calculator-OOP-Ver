8f2bed95731b89e8803e706181325d9e1e09ba64
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-21 11:48:36 +0800

    Exception File: exceptions added

--- exception_file.py
+++ exception_file.py
@@ -1,6 +1,13 @@
 #This class file will include the exception handling codes.
 
 #Create Class
+class Exceptions:
 #Create Object
+    def except_condition(exception):
 #Create instance variables
-#Return Values
+        if isinstance(exception, ZeroDivisionError):
+            print("Sorry! You are dividing by zero. Try changing the second number.")
+        elif isinstance(exception, ValueError):
+            print("Invalid input: Please input numbers only")
+        elif isinstance(exception):
+            print("Sorry, input should only be +, -, *, or /d")             
