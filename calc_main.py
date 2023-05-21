87bc523275ca8c847ce9b49ab39beb04e94dde45
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-21 12:15:12 +0800

    Will fix try-catch ....

--- calc_main.py
+++ calc_main.py
@@ -18,21 +18,24 @@ exception = Exceptions()
 
 retry = 'y'
 while loop.retry_prog(retry): 
-    #Ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication and Division)
-    operation = ui.operation_input() 
+    try: 
+        #Ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication and Division)
+        operation = ui.operation_input() 
+        exception.raise_error(errors)
+        #Ask the user for two numbers
+        num1 = ui.num_input()
+        num2 = ui.num_input()
 
-    #Ask the user for two numbers
-    num1 = ui.num_input()
-    num2 = ui.num_input()
+        #Perform the operation chosen with the two numbers
+        result = calc.calculations(operation, num1, num2)
 
-    #Perform the operation chosen with the two numbers
-    result = calc.calculations(operation, num1, num2)
+        #Display the result
+        ui.display_result(result)
 
-    #Display the result
-    ui.display_result(result)
-
-    #Ask if the user wants to try again or not.
-    retry = ui.retry_option()
+        #Ask if the user wants to try again or not.
+        retry = ui.retry_option()
 #If yes, repeat Step 1.
 #If no, Display “Thank you!” and the program will exit 
 #Use Python Function and appropriate Exceptions to capture errors during runtime.
+    except Exception as errors:
+        exception.except_condition(errors)
--- exception_file.py
+++ exception_file.py
@@ -3,11 +3,17 @@
 #Create Class
 class Exceptions:
 #Create Object
-    def except_condition(exception):
+    def except_condition(self, exception):
 #Create instance variables
         if isinstance(exception, ZeroDivisionError):
             print("Sorry! You are dividing by zero. Try changing the second number.")
         elif isinstance(exception, ValueError):
             print("Invalid input: Please input numbers only")
-        elif isinstance(exception):
-            print("Sorry, input should only be +, -, *, or /d")             
+        else:
+            print("Please enter a valid input")
+    def raise_error(self, operation):
+        operators = ["addition", "subtraction", "multiplication", "division"]
+        if  operation not in operators:
+            print("Sorry, operation should only be addition, subtraction, multiplication, or division")
+            
+    
