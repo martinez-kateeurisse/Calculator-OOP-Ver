e1d9a02e50a46337d374c4b0dbb3d31da2c9670c
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-06-03 15:49:56 +0800

    ui_mod file: displaying result method - added

--- ui_modified.py
+++ ui_modified.py
@@ -4,6 +4,8 @@ from user_interface import UserInterface
 #Create class
 class UiModified(UserInterface):    
 #Add methods
+    def display_result(self, result):
+        print(f"{Fore.LIGHTYELLOW_EX}RESULTING VALUE: " + str(result) + Fore.RESET)
 #Return results
 
 
