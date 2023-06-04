a5c3092539f7180efd3db7a37641f5d3783db447
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-06-04 13:06:07 +0800

    calc_ mod file: determining inequalities (try)

--- calculation_modified.py
+++ calculation_modified.py
@@ -5,9 +5,24 @@ from calculation_file import Calculator
 class CalcModified(Calculator):   
 #Add methods
     def calculations (self, operation, num1, num2):
-        #Power
+        #Power operation
         if operation == "^":
         #Perform operation
             result = num1**num2
         #Return result
             return result
+        
+        #Determining numbers are greater, lesser or equal
+        if operation == "=":
+            #If num 1 is greater
+            if num1 > num2:
+                result = str(num1) + "is greater than" + str(num2)
+                return result
+            #If num 2 is greater
+            elif num1 < num2:
+                result = str(num1) + "is less than" + str(num2)
+                return result
+            #If equal
+            else:
+                result = "The numbers you've entered are equal"
+                return result
